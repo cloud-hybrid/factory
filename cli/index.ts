4c58a6fe03686a8e5adb589068422c50798c9056
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-19 23:59:11 -0600

    [Development] - Types

--- cli/index.ts
+++
@@ -1,6 +0,0 @@
-const CLI = async () => await (await import("@cloud-vault/cli")).Main();
-
-export { CLI };
-
-export default CLI();
-
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,11 +1,15 @@
-export * from "@cloud-vault/cli";
+// export * from "@cloud-vault/cli";
 
 export * from "@cloud-vault/cli/commands";
 export * from "@cloud-vault/cli/utilities";
 export * from "@cloud-vault/cli/arguments";
 export * from "@cloud-vault/cli/header";
 
-const CLI = await import("./cli.js");
+interface CLI {
+    Main: typeof import("./cli.js").Main
+}
+
+const CLI: CLI = await import("./cli.js");
 
 export { CLI };
 
