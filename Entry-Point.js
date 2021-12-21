6d70cb9773f94473c56b1199f194bbd553ba67aa
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-21 03:13:24 -0600

    [Development] - Update

--- Entry-Point.js
+++ Entry-Point.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+import Subprocess  from "child_process";
+
+Subprocess.execSync("npm install", )
+
 import { default as CLI } from "./cli/src/index.js";
 
 const Main = async () => await CLI;
