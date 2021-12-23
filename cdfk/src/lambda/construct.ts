00e8c376551c22817dac41282fb7902d83bf1a6c
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-22 19:00:25 -0600

    [Development] - Templates, Source, Clean-Up

---
+++ cdfk/src/lambda/construct.ts
@@ -0,0 +1,143 @@
+/***
+ * @author      Jacob B. Sanders
+ * @package     @cloud-technology
+ * @summary     ESM Exportable Module
+ *
+ * @license     BSD 3-Clause License
+ * @copyright   Cloud-Technology LLC. & Affiliates
+ */
+
+import FS from "fs";
+import Path from "path";
+import Module from "module";
+
+import Assertion from "assert";
+
+import { Lambda } from "./function.js";
+
+/*** ESM Resolver for *Current-Working-Directory* */
+const CWD: string = Path.dirname(import.meta.url.replace("file" + ":" + "//", ""));
+
+/*** ESM Resolver for Package Directory relative to Current Working Directory */
+const PKG: string = Path.dirname(CWD);
+
+/*** ESM Resolver for Source Directory relative to the Current Working Directory */
+const Source: string = Path.dirname(PKG);
+
+/*** ESM Resolver for Schemas Directory relative to the Current Working Directory */
+const Schemas: string = Path.dirname(Source);
+
+/*** ESM Compatability & JSON Importer */
+const Import: NodeRequire = Module.createRequire(import.meta.url);
+
+/// --- --- --- /// --- --- --- /// --- --- --- /// --- --- --- /// --- --- --- /// --- --- --- /// --- --- --- ///
+
+/***
+ * Parent Directory Name Resolver
+ * ------------------------------
+ *
+ * @param path {String|FS.PathLike} Directory or File System Path
+ *
+ * @constructor
+ *
+ */
+
+const Parent = (path: string) => {
+    const Resolved = Path.dirname(path);
+    return {
+        ...Path.parse(Resolved), ...{
+            $: FS.readdirSync(Resolved, {withFileTypes: true})
+        }
+    };
+}
+
+/*** Expected: ~ factory/cdfk */
+const directory: string = Parent(CWD).dir
+
+/*** Expected: ~ factory */
+const Package = Parent(directory);
+Assertion.strictEqual(Package.name, "factory");
+
+/*** Expected: "packages" Directory in Package Folder */
+const Descriptors = Package.$.map(($) => $.name)
+Assertion.strictEqual(Descriptors.includes("packages"), true);
+
+/*** Expected: "packages" Directory */
+const Packages = Path.join(Path.format(Package), "packages");
+Assertion.strictEqual(FS.existsSync(Packages), true);
+
+/***
+ * Lambda Packaging Construct
+ * --------------------------
+ * The minimal following folder structure is a requirement to deploy a lambda:
+ *
+ * ```
+ * factory
+ *  └── packages
+ *       ├── artifacts.json
+ *       └── service
+ *            └── lambda-(n + 1)
+ *                 ├── package.json
+ *                 └── src
+ *                      └── index.js
+ * ```
+ *
+ * Such requirements derive from the following assumptions:
+ *      - A) **The lambda function is to be readily available over HTTP protocol**
+ *          * At a minimum, a single API Gateway is required
+ *      - B) **The lambda function is to be REST-compliant** (without state)
+ *          * Any given lambda functions cannot require the state or source dependencies of another
+ *      - C) **The lambda function is to be publicly accessible** (authentication being arbitrary)
+ *          * Arguably an extension of point A
+ *      - D) **The lambda function is to be deployable as *apart of a larger service, or individually***
+ *          * Development lifecycles see benefit in this flexibility; deployment runtimes can easily
+ *            and expectedly clone in any given repository into a target directory
+ *
+ * Where `artifacts.json` defines the folder structure and runtime requirements in order for pipelines
+ * to compile such compatible folder structure(s). Objectively, it can be assumed that such information that
+ * gets written into `artifacts.json` is provided by either package maintainers or a development team(s).
+ *
+ */
+
+class Construct {
+    /*** Target Deployable Lambda Layers Check */
+    layers?: boolean;
+
+    /*** Target Deployable Lambda Service Folder Check */
+    service?: boolean;
+
+    /*** Target Deployable Lambda `artifacts.json` Check */
+    artifacts?: boolean;
+
+    private static readonly packages: string = Packages;
+
+    constructor() {
+        this.artifacts = Construct.includes("artifacts.json");
+        this.service = Construct.includes("service");
+        this.layers = Construct.includes("layers");
+    }
+
+    private static includes(name: string) {
+        return FS.readdirSync(Construct.packages, {withFileTypes: false})
+            .map((descriptor) => descriptor).includes(name);
+    }
+
+    Artifacts() {}
+
+
+}
+
+/// Transform Class Types into Functional Compositions
+const Implementation: typeof Construct = new Proxy(Construct, {
+    apply (target: Construct | any, type, parameters: any[]) {
+        return target(... parameters);
+    }
+});
+
+interface Implementation extends Construct {}
+const Interface: () => Implementation = () => {
+    return new Implementation();
+}
+
+export default Interface;
+
