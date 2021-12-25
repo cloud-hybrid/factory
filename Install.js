2eefb093859bd2505659c89e9abd770cb630c191
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-25 07:40:04 -0600

    [Development] - Update

--- Install.js
+++ Install.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+import Path from "path";
 import Subprocess from "child_process";
 
-Subprocess.spawnSync("npm", ["install", "."], { shell: true, stdio: "inherit", cwd: "./cli" });
-Subprocess.spawnSync("tsc", ["--build", "--pretty"], { shell: true, stdio: "inherit", cwd: "./cli" });
+const CWD = Path.dirname(import.meta.url.replace("file" + ":" + "//", ""));
 
-Subprocess.spawnSync("npm", ["install", "."], { shell: true, stdio: "inherit", cwd: "./cdfk" });
-Subprocess.spawnSync("tsc", ["--build", "--pretty"], { shell: true, stdio: "inherit", cwd: "./cdfk" });
+Subprocess.spawnSync("npm", ["install", "--silent"], { shell: true, stdio: "inherit", cwd: Path.join(CWD, "cli") });
+Subprocess.spawnSync("tsc", ["--build", "--pretty"], { shell: true, stdio: "inherit", cwd: Path.join(CWD, "cli") });
 
-Subprocess.spawnSync("npm", ["install", "."], { shell: true, stdio: "inherit", cwd: "." });
+Subprocess.spawnSync("npm", ["install", "--silent"], { shell: true, stdio: "inherit", cwd: Path.join(CWD, "cdfk") });
+Subprocess.spawnSync("tsc", ["--build", "--pretty"], { shell: true, stdio: "inherit", cwd: Path.join(CWD, "cdfk") });
+
+Subprocess.spawnSync("npm", ["install", "."], { shell: true, stdio: "inherit", cwd: CWD });
