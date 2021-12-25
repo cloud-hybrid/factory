b05e9d17dea80bee843501197d8d8ff7c289ee15
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-12-25 07:24:53 -0600

    [Development] - Update

--- Install.js
+++ Install.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
-import Path from "path";
 import Subprocess from "child_process";
-
-Subprocess.spawnSync("npm", ["install", "--global", "typescript"], { shell: true, stdio: "inherit" });
 
 Subprocess.spawnSync("npm", ["install", "."], { shell: true, stdio: "inherit", cwd: "./cli" });
 Subprocess.spawnSync("tsc", ["--build", "--pretty"], { shell: true, stdio: "inherit", cwd: "./cli" });
@@ -12,7 +9,3 @@ Subprocess.spawnSync("npm", ["install", "."], { shell: true, stdio: "inherit", c
 Subprocess.spawnSync("tsc", ["--build", "--pretty"], { shell: true, stdio: "inherit", cwd: "./cdfk" });
 
 Subprocess.spawnSync("npm", ["install", "."], { shell: true, stdio: "inherit", cwd: "." });
-
-//await import("@cloud-vault/cli");
-//
-////import { default as CLI } from "@cloud-vault/cli";
