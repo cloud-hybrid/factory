a9e3f83eff1f81ddb174b1bd6d7ced43792daa00
Jacob B. Sanders <jacob.sanders@cloudhybrid.io>
2022-01-07 03:44:35 -0600

    [Development] - CLI Colrs & Header

--- src/cli/header.ts
+++ src/cli/header.ts
@@ -2,10 +2,10 @@ import Colors from "chalk";
 
 const Year = new Date().getFullYear();
 
-const Title = Colors.bold.underline("Cloud-Factory - Construct CDK");
-const Description = Colors.italic("Deployment Extensions & Cloud Utilities");
+const Title = Colors.bold.underline("Cloud-Factory") + " - " + Colors.italic.red("Software-Defined State");
+const Description = Colors.white("CLI Tools & Cloud Management Utilities");
 const Copyright = Colors.dim("Open Source" + "," + " " + Year);
-const License = Colors.dim("BSD-3 License");
+const License = Colors.dim("BSD-1 License");
 
 const Generate = () => {
     const Lines: string[] = [];
@@ -13,15 +13,15 @@ const Generate = () => {
     /// Check if Local `run-script start` is interfacing `factory`
     (process.env?.npm_lifecycle_event !== "start") && Lines.push("");
 
-    Lines.push([Colors.blue("    //\\\\--/"), "  ", Colors.magenta(" //\\\\--/")].join(""));
-    Lines.push([Colors.blue("   ╱╱__\\\\/"), "  ", Colors.magenta(" //__\\\\/")].join("") + "   " + Title);
-    Lines.push([Colors.blue("  ╱╱\\\\--/"), "  ", Colors.magenta(" //\\\\--/")].join(""));
-    Lines.push([Colors.blue(" //__\\\\/"), "  ", Colors.magenta(" //__\\\\/")].join("") + "   " + Description);
-    Lines.push([Colors.blue(" \\\\--/\\\\"), "  ", Colors.magenta("//\\\\--/")].join(""));
-    Lines.push([Colors.blue("  \\\\/__\\\\"), "", Colors.magenta("//__\\\\/")].join("") + "   " + Copyright + " " + License);
-    Lines.push([Colors.blue("   \\\\---"), "", Colors.magenta("//\\\\--/")].join(""));
-    Lines.push([Colors.blue("    \\\\"), " ", Colors.magenta("//__\\\\/")].join("") + "   " + "--[H]elp for Usage");
-    Lines.push([Colors.blue("     \\\\"), "", Colors.magenta("\\\\--//")].join(""));
+    Lines.push([Colors.blue("    //\\\\--/"), "  ", Colors.magentaBright(" //\\\\--/")].join(""));
+    Lines.push([Colors.blue("   ╱╱__\\\\/"), "  ", Colors.magentaBright(" //__\\\\/")].join("") + "   " + Title);
+    Lines.push([Colors.blue("  ╱╱\\\\--/"), "  ", Colors.magentaBright(" //\\\\--/")].join(""));
+    Lines.push([Colors.blue(" //__\\\\/"), "  ", Colors.magentaBright(" //__\\\\/")].join("") + "   " + Description);
+    Lines.push([Colors.blue(" \\\\--/\\\\"), "  ", Colors.magentaBright("//\\\\--/")].join(""));
+    Lines.push([Colors.blue("  \\\\/__\\\\"), "", Colors.magentaBright("//__\\\\/")].join("") + "   " + Copyright + " " + License);
+    Lines.push([Colors.blue("   \\\\---"), "", Colors.magentaBright("//\\\\--/")].join(""));
+    Lines.push([Colors.blue("    \\\\"), " ", Colors.magentaBright("//__\\\\/")].join("") + "   " + Colors.yellow("--[H]elp for Usage"));
+    Lines.push([Colors.blue("     \\\\"), "", Colors.magentaBright("\\\\--//")].join(""));
     Lines.push("");
 
     return Lines.join("\n");
